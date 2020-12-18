1c56400f47fef9b52c8f80aff50c784e1b981279
micky0singh <38345454+micky0singh@users.noreply.github.com>
2020-12-19 01:37:10 +0530

    Update helpme.py

--- helpme/helpme.py
+++ helpme/helpme.py
@@ -15,3 +15,10 @@ class helpme(commands.Cog):
         # Your code will go here
 
         await ctx.send("Click on this link below to get list of all bot commands.\n\nhttps://github.com/micky0singh/Elixir-Bot-Guide/wiki")
+
+            embed = discord.Embed(
+                title="Error",
+                description="Modmail functioning guild not found.",
+                color=self.bot.error_color,
+            )
+            await ctx.send(embed=embed)
